5aa45b6c1c4c6af5ccb203d946977670b7ae2b8a
Emmanuel <emmanuelreyna0410@gmail.com>
2021-07-14 19:46:16 -0500

    Se agrego el local storage para el inicio de sesión

--- assets/js/loginUsuario.js
+++ assets/js/loginUsuario.js
@@ -1,17 +1,32 @@
-const user = document.getElementById('user');
-const pass = document.getElementById('pass');
+// Obtenemos los elementos del html mediante el uso de DOM
+const form = document.getElementById('form');
+const username = document.getElementById('user');
+const password = document.getElementById('password');
 const warnings = document.getElementById('warnings');
 
 
-ingresar.addEventListener("click", e=>{
-    e.preventDefault();
+form.addEventListener('submit', event=>{
+    event.preventDefault();
     let usuarioPrueba = "Usuario";
     let contraseña = "12345678";
     let entrar = false;
     let mensaje = "";
 
-    if (user.value == usuarioPrueba && pass.value == contraseña){
-        window.location.href = 'index-general.html';
+    let users = Array(
+        {
+            usuario: username.value,
+            contraseña: password.value
+        }
+    );
+
+    if (username.value == usuarioPrueba && password.value == contraseña){
+        localStorage.setItem('user', JSON.stringify(users));
+
+        location.href = 'index-general.html';
+        /* En localStorage.setItem ingresamos dos parametros, el primero es la clave con la que 
+        luego van a ser llamados los datos y el segundo parametro son los datos, sin embargo
+        localStorage solo recibe string así que hacemos la conversion mediante JSON.stringify() */
+        
     } else {
         mensaje = "Usuario o contraseña incorrectos";
         entrar = true;
